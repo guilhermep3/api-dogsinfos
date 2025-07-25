e8b0c05104ff5b7cdc8a7a76e9e41216cd2b36a3
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-07-25 14:31:23 -0300

    add more 10 dogs

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import { getDogs } from "../models/dog";
+import z from "zod";
+import { details } from "./pageController";
 
+const searchSchema = z.object({
+  q: z.string().min(2, "A busca precisa ter pelo menos 2 caracteres"),
+});
 export const search = (req: Request, res: Response) => {
-  let query: string = req.query.q as string;
+  // let query: string = req.query.q as string;
+  const result = searchSchema.safeParse(req.query);
 
-  if (!query) {
-    res.status(400).json({ error: 'Informe uma raça para buscar.' });
+  if (!result.success) {
+    res.status(400).json({
+      error: 'Informe uma raça para buscar.',
+      details: result.error
+    });
     return;
   };
 
+  const query = result.data.q
   let list = getDogs.getFromBreed(query);
 
   if (list.length === 0) {
